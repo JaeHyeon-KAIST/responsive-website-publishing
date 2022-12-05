47270704fd1a0a1c3702a7e4f1e427dfa1acecfa
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2022-12-05 22:34:14 +0900

    Project Section

--- custom.js
+++ custom.js
@@ -27,5 +27,16 @@ $(function(){
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 2
-  })
+  });
+
+   // slick.js : project photo
+  $('.project-photo').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear'
+  });
 })
